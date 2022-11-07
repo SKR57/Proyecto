14e62d67aefdcde87962526eb143a60e9a69b159
SKR57 <Skroll57@outlook.com>
2022-11-07 00:37:17 -0600

    Initial commit

---
+++ src/components/Chat.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import Cameraicon from '../pages/Estilos/Home/Images/video-camera.png';
+import Moreicon from '../pages/Estilos/Home/Images/plus.png';
+import Messages from './Messages';
+import Input from './Input';
+import { ChatContext } from '../context/ChatContext';
+
+function Chat() {
+  const {data} = useContext(ChatContext);
+
+  return (
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>{data.user?.displayName}</span>
+        <div className="chatIcons">
+          <img src={Cameraicon} alt=''/>
+          <img src={Moreicon} alt=''/>
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
+  )
+}
+
+export default Chat
